8371ae3d0ed4f71f91ddd2d157fc2afd84979145
traversc <traversc@users.noreply.github.com>
2016-04-25 14:08:51 -1000

    Update gmail-reply-at-top.js

--- gmail-reply-at-top.js
+++ gmail-reply-at-top.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name     Gmail reply at top
-// @namespace     http://mail.google.com
-// @require  http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js
-// @require  https://gist.github.com/raw/2625891/waitForKeyElements.js
+// @include     https://mail.google.com/*
+// @require http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
+// @require  https://gist.githubusercontent.com/BrockA/2625891/raw/waitForKeyElements.js
 // @grant    GM_addStyle
 // ==/UserScript==
 /*- The @grant directive is needed to work around a major design
@@ -13,7 +13,6 @@
 waitForKeyElements (".gB.acO", actionFunction);
 
 function actionFunction (jNode) {
-    //jNode.css ("background", "yellow"); // example
     replydiv = jNode.parent();
     replydiv.prependTo(replydiv.parent()); 
     setTimeout(function () {$(".Tm.aeJ").scrollTop(0);}, 500);
